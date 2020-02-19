9b752aa13e853bb9cc7aaf23a28754f5d7f9ba0d
Matthieu <matthieunadeau96@gmail.com>
2020-02-19 17:52:43 -0500

    Step 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var textIndex = 0;
+  void buttonPressed() {
+    setState(() {
+      textIndex = textIndex + 1;
+    });
+    print(textIndex);
+  }
+
+  @override
+  var textList = ['hello', 'hi', 'hey'];
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('First Assignment'),
         ),
-        body: Text('Some Text!'),
+        body: Column(
+          children: [
+            Text(textList[textIndex]),
+            RaisedButton(
+              onPressed: buttonPressed,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
